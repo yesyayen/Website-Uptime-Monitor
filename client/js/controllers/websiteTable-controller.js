4f00607822fd2406f61c53cdd08f56f3f8021409
yesyayen <yesyayen@gmail.com>
2015-07-16 02:53:24 +0530

    added [BASIC]socket.io to enable real-time status update of the servers

--- client/js/controllers/websiteTable-controller.js
+++ client/js/controllers/websiteTable-controller.js
@@ -1,8 +1,44 @@
-app2.controller('websiteTable', ['$scope', '$resource', function ($scope, $resource) {
+app2.factory('socket', function($rootScope) {
+	var socket = io.connect();
+	return {
+		on: function(eventName, callback) {
+			socket.on(eventName, function() {
+				var args = arguments;
+				$rootScope.$apply(function() {
+					callback.apply(socket, args);
+				});
+			});
+		},
+		emit: function(eventName, data, callback) {
+			socket.emit(eventName, data, function() {
+				var args = arguments;
+				$rootScope.$apply(function() {
+					if(callback) {
+						callback.apply(socket, args);
+					}
+				});
+			});
+		}
+	};
+});
+
+
+app2.controller('websiteTable', ['$scope', '$resource', 'socket', function ($scope, $resource, socket) {
 	var Monitor = $resource('/api/websites');
 
 	Monitor.query(function (results) {
-		console.log(results);
 		$scope.webResult = results;
 	});
+	
+	// Incoming
+	socket.on('onNoteCreated', function(data) {
+		$scope.webStatus = data;
+		if($scope.webResult != undefined)
+		{
+			$scope.webResult[$scope.webResult.length - 1].isUp = "true";
+		}	
+	});
+
+	// Outgoing - to server
+	socket.emit('createNote', 'note');
 }]);
