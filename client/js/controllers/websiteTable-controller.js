00ca4b303468e5f7b847f36969f3b95c98c85b15
yesyayen <yesyayen@gmail.com>
2015-07-15 11:10:21 +0530

    added node-libcurl - executing with sample URL

--- client/js/controllers/websiteTable-controller.js
+++ client/js/controllers/websiteTable-controller.js
@@ -1,12 +1,8 @@
 app2.controller('websiteTable', ['$scope', '$resource', function ($scope, $resource) {
 	var Monitor = $resource('/api/websites');
 
-	$scope.asdf = "asd";
-
 	Monitor.query(function (results) {
 		console.log(results);
 		$scope.webResult = results;
 	});
-
-
 }]);
