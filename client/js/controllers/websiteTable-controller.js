eb73972d2a298b6d0db8225ffa949c8823e93139
yesyayen <yesyayen@gmail.com>
2015-07-17 14:47:58 +0530

    remove website feature, removes the data of that website from all collections

--- client/js/controllers/websiteTable-controller.js
+++ client/js/controllers/websiteTable-controller.js
@@ -1,60 +1,65 @@
 app2.factory('socket', function($rootScope) {
-	var socket = io.connect();
-	return {
-		on: function(eventName, callback) {
-			socket.on(eventName, function() {
-				var args = arguments;
-				$rootScope.$apply(function() {
-					callback.apply(socket, args);
-				});
-			});
-		},
-		emit: function(eventName, data, callback) {
-			socket.emit(eventName, data, function() {
-				var args = arguments;
-				$rootScope.$apply(function() {
-					if(callback) {
-						callback.apply(socket, args);
-					}
-				});
-			});
-		}
-	};
+    var socket = io.connect();
+    return {
+        on: function(eventName, callback) {
+            socket.on(eventName, function() {
+                var args = arguments;
+                $rootScope.$apply(function() {
+                    callback.apply(socket, args);
+                });
+            });
+        },
+        emit: function(eventName, data, callback) {
+            socket.emit(eventName, data, function() {
+                var args = arguments;
+                $rootScope.$apply(function() {
+                    if (callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            });
+        }
+    };
 });
 
 
-app2.controller('websiteTable', ['$scope', '$resource', 'socket', function ($scope, $resource, socket) {
-	var Monitor = $resource('/api/websites');
+app2.controller('websiteTable', ['$scope', '$resource', 'socket', function($scope, $resource, socket) {
+    var Monitor = $resource('/api/websites');
 
-	Monitor.query(function (results) {
-		$scope.webResult = results;
-	});
-	
-	// Incoming
-	socket.on('websiteStatus', function(data) {
-		console.log($scope.webResult);
-		var i;
-		if($scope.webResult != undefined)
-		{
-			for(i = 0; i < $scope.webResult.length; i++)
-			{
-				console.log($scope.webResult[i].name);
-				console.log(data.name);
-				if($scope.webResult[i].name.localeCompare(data.name) == 0)
-				{
-					break;
-				} 
-			}
-			$scope.webResult[i].statusCode = data.statusCode;
-			$scope.webResult[i].isUp = data.isUp;
-			$scope.webResult[i].responseTime = data.responseTime;
-		}	
-	});
+    Monitor.query(function(results) {
+        $scope.webResult = results;
+    });
 
-	socket.on('shabbaaaa', function(data) {
-		console.log("adasdasdasdasddadaasdsadadasd");	
-	});
+    // remove user
+    $scope.removeUser = function(index) {
+        socket.emit('deleteWebsite', $scope.webResult[index].name);
+        console.log(index + "   " + $scope.webResult[index].name);
+        $scope.webResult.splice(index, 1);
+    };
 
-	// Outgoing - to server
-	socket.emit('createNote', 'note');
+    // Incoming
+    socket.on('websiteStatus', function(data) {
+        console.log($scope.webResult);
+        var i;
+        if ($scope.webResult != undefined) {
+            for (i = 0; i < $scope.webResult.length; i++) {
+                console.log($scope.webResult[i].name);
+                console.log(data.name);
+                if ($scope.webResult[i].name.localeCompare(data.name) == 0) {
+                    break;
+                }
+            }
+            
+            $scope.webResult[i].statusCode = data.statusCode;
+            $scope.webResult[i].isUp = data.isUp;
+            $scope.webResult[i].responseTime = data.responseTime;
+        }
+    });
+
+    socket.on('shabbaaaa', function(data) {
+        console.log("adasdasdasdasddadaasdsadadasd");
+    });
+
+    // Outgoing - to server
+    socket.emit('createNote', 'note');
 }]);
--- server/routes/socket.js
+++ server/routes/socket.js
@@ -1,4 +1,8 @@
-var sockets = {}
+var Monitor   = require('../models/websites');
+var Ping = require('../models/pings');
+var websiteListController = require('../controllers/websitelist-controller');
+
+var sockets = {};
 module.exports = function(socket) {
     //You can declare all of your socket listeners in this file, but it's not required 
 
@@ -8,8 +12,26 @@ module.exports = function(socket) {
         console.log(data);
     });
 
+    socket.on('deleteWebsite', function(data) {
+    	removeWebsite(data);
+        console.log(data);
+    });
+
     socket.on('disconnect', function() {
         console.log("Socket disconnect!");
         delete sockets[socket.id];
     });
 };
+
+function removeWebsite(name)
+{
+    console.log(websiteListController.removeFromTimer(name));
+	Monitor.findOne({'name': name}, function(err, results) {
+        websiteID = results._id;
+        console.log(websiteID);
+        Ping.remove({'name' : name}, function(error, result) { 
+            console.log(result);
+        });
+        results.remove();
+    });
+}
