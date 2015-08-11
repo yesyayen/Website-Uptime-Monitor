4807d1b2d52b457c967d711f49b03669242c679f
Anandh Somasundaram <yesyayen@gmail.com>
2015-08-11 10:00:12 -0400

    added config file

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {};
+
+config.gmail = {};
+config.redis = {};
+config.web = {};
+
+config.gmail.user_name = 'your gmail id';
+config.gmail.password =  'gmail password';
+
+module.exports = config;
